76a284430457b6b9541a49483ea34d2369ef5285
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-12-28 14:13:58 -0800

    moves functions into global scope

--- script.js
+++ script.js
@@ -1,78 +1,81 @@
-async function getJSON() {
-  const res = await fetch(
-    `https://data.nba.com/data/v2015/json/mobile_teams/nba/2020/teams/trail_blazers_schedule_02.json`
+const API_URL = `https://data.nba.com/data/v2015/json/mobile_teams/nba/2020/teams/trail_blazers_schedule_02.json`;
+
+// Utils
+// ===========================
+
+// regex to remove unneeded characters from JSON date at gscd.g.gdte
+const removeDashAndChar = /[^a-zA-Z0-9 ]/g;
+
+const formatTime = (time) =>
+  time.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+// adds ordinal (st, nd, rd, th) to day
+const dateOrdinal = (date) => {
+  return (
+    date +
+    (31 == date || 21 == date || 1 == date
+      ? 'st'
+      : 22 == date || 2 == date
+      ? 'nd'
+      : 23 == date || 3 == date
+      ? 'rd'
+      : 'th')
   );
+};
 
-  const data = await res.json();
-
+// Trail Blazers Schedule Widget
+// ===========================
+async function main() {
   // destructors gscd out of initial JSON response object
-  const { gscd } = data;
+  const { gscd } = await fetch(API_URL).then((res) => res.json());
 
   // gets current date and reformats it
   const now = new Date();
   const today =
     `${now.getFullYear()}` + `${now.getMonth() + 1}` + `${now.getDate()}`;
 
-  // regex to remove unneeded characters from JSON date at gscd.g.gdte
-  const removeDashAndChar = /[^a-zA-Z0-9 ]/g;
   // creates an new array with all past games removed
   const futureGames = gscd.g.filter(
-    (i) => i.gdte.replace(removeDashAndChar, '') >= today
+    (item) => item.gdte.replace(removeDashAndChar, '') >= today
   );
 
   // compares first game in the futureGames array with todays date
   // to determine if it is GAMEDAY
-  const GAMEDAY =
-    futureGames[0].gdte.replace(removeDashAndChar, '') === today ? true : false;
+  const GAMEDAY = futureGames[0].gdte.replace(removeDashAndChar, '') === today;
 
-  // changes display formatting of date
+  // changes display formatting of date and ensures that PST time
+  // is always displayed
 
-  const gameDay = new Date(futureGames[0].htm);
-  const nextGame_1 = new Date(futureGames[1].htm);
-  const nextGame_2 = new Date(futureGames[2].htm);
-  // console.log('nextGame:', nextGame_1);
-  // console.log('futureGames', futureGames);
+  const gameDay =
+    // checks to see if the game is being played in Portland
+    futureGames[0].ac === 'Portland'
+      ? new Date(futureGames[0].htm)
+      : new Date(futureGames[0].vtm);
 
-  timeOptions = {
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true,
-    timeZone: 'America/Los_Angeles',
-  };
+  const nextGame_1 =
+    futureGames[1].ac === 'Portland'
+      ? new Date(futureGames[1].htm)
+      : new Date(futureGames[1].vtm);
 
-  let timeConverter = (time) =>
-    new Intl.DateTimeFormat('en-US', timeOptions).format(time);
-
-  // adds ordinal (st, nd, rd, th) to day
-  function dateOrdinal(date) {
-    return (
-      date +
-      (31 == date || 21 == date || 1 == date
-        ? 'st'
-        : 22 == date || 2 == date
-        ? 'nd'
-        : 23 == date || 3 == date
-        ? 'rd'
-        : 'th')
-    );
-  }
-
-  const options = {
-    weekday: 'long',
-  };
+  const nextGame_2 =
+    futureGames[2].ac === 'Portland'
+      ? new Date(futureGames[2].htm)
+      : new Date(futureGames[2].vtm);
 
   // formats game day date, time, and arena
-  const gameDayDate = `${gameDay.toLocaleDateString(
-    undefined,
-    options
-  )} ${dateOrdinal(gameDay.getDate())} / ${gameDay.toLocaleTimeString([], {
+  const gameDayDate = `${gameDay.toLocaleDateString(undefined, {
+    weekday: 'long',
+  })} ${dateOrdinal(gameDay.getDate())} / ${gameDay.toLocaleTimeString([], {
     timeStyle: 'short',
   })} @ ${futureGames[0].an}`;
 
   // evaluates if it is a Blazers home or away game
   // and saves the broadcast stations in an array
 
-  function getBroadcast(index) {
+  const getBroadcast = (index) => {
     let radio = [];
     let tv = [];
 
@@ -105,7 +108,7 @@ async function getJSON() {
       radio,
       tv,
     };
-  }
+  };
 
   // sets each days broadcast stations in a new array
   // to access, gameDayBroadcast.tv or gameDayBroadcast.radio
@@ -120,7 +123,7 @@ async function getJSON() {
     <p class="date">
      ${
        GAMEDAY
-         ? `<strong>TODAY</strong> / ${timeConverter(gameDay)} @ ${
+         ? `<strong>TODAY</strong> / ${formatTime(gameDay)} @ ${
              futureGames[0].an
            }`
          : gameDayDate
@@ -149,6 +152,8 @@ async function getJSON() {
 
   // renders matchup
   const matchup = document.getElementById('matchup');
+
+  // TODO: use hi-res images
   matchup.innerHTML = `
     <img src="https://www.nba.com/.element/img/1.0/teamsites/logos/teamlogos_80x64/${awayTeam[0]}.gif" alt="" style="width: 80px;"/>
     <p> @ </p>
@@ -158,7 +163,7 @@ async function getJSON() {
   // renders table
   const table = document.getElementById('upcomingGames');
   table.innerHTML = `
-    <tr onpointerdown="window.location='https://www.nba.com/blazers/schedule'" onkeydown="window.location='https://www.nba.com/blazers/schedule'" tabindex="0" aria-role="link" class="clickable">
+    <tr onpointerdown="window.location='https://www.nba.com/blazers/schedule'"  tabindex="0" aria-role="link" class="clickable">
       <td class="matchup-header">
         <div class="table_matchup_div">
           <img
@@ -179,7 +184,7 @@ async function getJSON() {
           <p>${homeTeam[1].toUpperCase()}</p>
         </div>
       </td>
-      <td class="table-time-slot"> ${timeConverter(nextGame_1)}</td>
+      <td class="table-time-slot"> ${formatTime(nextGame_1)}</td>
       <td>${`${nextGame_1.getMonth() + 1}/${nextGame_1.getDate()}`}</td>
       <td class="padding_right">${nextGameBroadcast1.tv[0].disp}</td>
     </tr>
@@ -204,11 +209,11 @@ async function getJSON() {
           <p>${homeTeam[2].toUpperCase()}</p>
         </div>
       </td>
-      <td class="table-time-slot"> ${timeConverter(nextGame_2)}</td>
+      <td class="table-time-slot"> ${formatTime(nextGame_2)}</td>
       <td>${`${nextGame_2.getMonth() + 1}/${nextGame_2.getDate()}`}</td>
       <td class="padding_right">${nextGameBroadcast2.tv[0].disp}</td>
     </tr>
   `;
 }
 
-getJSON();
+main();
